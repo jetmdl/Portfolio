5dd5c92c2d33211d9d2d5f90be967f9004d93013
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-05-27 14:13:18 +0100

    removed unused files

--- src/addEventListners.js
+++
@@ -1,5 +0,0 @@
-export default function addMenuListeners(){
-    document.querySelector("#menuHomeButton").addEventListener('click', goToHome);
-    document.querySelector("#menuMenuButton").addEventListener('click', goToMenu);
-    document.querySelector("#menuContactButton").addEventListener('click', goToContact);
-}
--- src/removeItems.js
+++
@@ -1,6 +0,0 @@
-export default function removeElementsFromDefined(anId){
-    const el = document.getElementById(anId);
-    while(el.firstChild) {
-        el.removeChild(el.lastChild);
-    }
-}
